5cbc0dae15bf8e26af949ef4613ca1e52f23b8d9
Oscar Zhang <oscarzhang228@gmail.com>
2024-03-02 15:02:50 -0500

    change layout of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,14 +7,14 @@ import styles from "./Footer.module.scss";
  */
 export default function Footer() {
   return (
-    <div className="d-flex justify-content-center p-md-4">
+    <div className="d-flex justify-content-center p-md-4 flex-column align-items-center">
       <img
         src={GTLogo}
-        className={styles["Footer-Logo"]}
+        className={`${styles["Footer-Logo"]} pt-lg-5 pt-3`}
         alt="Georgia Tech Logo"
       ></img>
       <div className="d-flex flex-column justify-content-center">
-        <h1 className={styles["Footer-Heading"]}>
+        <h1 className={`${styles["Footer-Heading"]} px-5 py-2 text-center`}>
           Social Dynamics and Well-Being Lab @ Georgia Tech
         </h1>
       </div>
